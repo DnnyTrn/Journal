84eadd25da97b2ea91a9df5c1f3f84cd0314e789
Danny Tran <dannyytran@gmail.com>
2021-03-01 12:26:45 -0800

    styling for title

--- styles.dart
+++ styles.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class Styles {
   static final lightTheme = ThemeData.light();
   static final darkTheme = ThemeData.dark();
-  static final titleStyle = TextStyle(color: Colors.red);
+  static final titleStyle =
+      TextStyle(fontWeight: FontWeight.bold, fontSize: 40);
   static final dateStyle = TextStyle();
-  static final bodyStyle = TextStyle();
+  static final bodyStyle = TextStyle(fontSize: 20);
 }
